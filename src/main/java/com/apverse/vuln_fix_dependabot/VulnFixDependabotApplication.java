ef04e82e7b4df0574b8496186959b786cb4c8f76
Anmol <anmol@Anmols-MacBook-Air.local>
2024-08-18 12:48:47 +0530

    init

---
+++ src/main/java/com/apverse/vuln_fix_dependabot/VulnFixDependabotApplication.java
@@ -0,0 +1,13 @@
+package com.apverse.vuln_fix_dependabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnFixDependabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnFixDependabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apverse/vuln_fix_dependabot/VulnFixDependabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apverse.vuln_fix_dependabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnFixDependabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
